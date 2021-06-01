8b1162cf285bf1a984d914d3583c87b7604201bc
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-06-01 17:18:52 -0300

    StockDTO

--- src/main/java/com/example/bootcamp/model/dto/StockDTO.java
+++ src/main/java/com/example/bootcamp/model/dto/StockDTO.java
@@ -1,5 +1,21 @@
 package com.example.bootcamp.model.dto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockDTO {
     
+    private Long id;
+    private String name;
+    private Double price;
+    private Double variation;
+    private LocalDate date;
 }
