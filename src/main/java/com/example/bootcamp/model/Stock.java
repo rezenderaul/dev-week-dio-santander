99dae6e04b8dcc49b92425fe9f5e221c4be84389
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-06-01 17:42:57 -0300

    Stock

---
+++ src/main/java/com/example/bootcamp/model/Stock.java
@@ -0,0 +1,41 @@
+package com.example.bootcamp.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_stock")
+public class Stock {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "price")
+    private Double price;
+    
+    @Column(name = "variation")
+    private Double variation;
+    
+    @Column(name = "date")
+    private LocalDate date;
+}
