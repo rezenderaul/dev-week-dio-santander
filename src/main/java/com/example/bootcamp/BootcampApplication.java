ae43f5cd47cfc4c9bdd51bdf8aa7fcfcf93889d4
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-06-01 20:37:47 -0300

    Update openAPI

--- src/main/java/com/example/bootcamp/BootcampApplication.java
+++ src/main/java/com/example/bootcamp/BootcampApplication.java
@@ -1,5 +1,6 @@
 package com.example.bootcamp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +17,11 @@ public class BootcampApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI() {
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description, 
+			@Value("${application.version}") String version) {
 		return new OpenAPI().info(new Info()
-			.title("")
-			.version("1.0")
+			.title(description)
+			.version(version)
 			.termsOfService("http://swagger.io/terms")
 			.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
