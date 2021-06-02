cd2ce31b6e5779219f559e11f3b19b6c7b1ca998
RAUL CRISTIANO REZENDE <raulcristianorezende@outlook.com.br>
2021-06-01 23:54:25 -0300

    Fix exceptions

--- src/main/java/com/example/bootcamp/exceptions/ExceptionsHandler.java
+++ src/main/java/com/example/bootcamp/exceptions/ExceptionsHandler.java
@@ -1,15 +1,13 @@
 package com.example.bootcamp.exceptions;
 
-import java.time.Instant;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.example.bootcamp.util.MessageUtils;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @ControllerAdvice
 public class ExceptionsHandler {
@@ -18,15 +16,30 @@ public class ExceptionsHandler {
     @ExceptionHandler(ResourceNotFound.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFound exception, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
-        return ResponseEntity.status(status).body(this.standard(status, MessageUtils.ENTITY_NOT_FOUND, request));
+        return ResponseEntity.status(status).body(this.standard(
+                status, exception.getMessage(), request, MessageUtils.ENTITY_NOT_FOUND));
     }
 
-    private StandardError standard(HttpStatus status, String error, HttpServletRequest request) {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<StandardError> handlerSecurity(ResourceNotFound exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        return ResponseEntity.status(status).body(this.standard(
+                status, exception.getMessage(), request, MessageUtils.STOCK_ALREADY_EXISTS));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardError> handlerSecurity(Exception exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(status).body(this.standard(
+                status, exception.getMessage(), request, MessageUtils.SERVER_ERROR));
+    }
+
+    private StandardError standard(HttpStatus status, String error, HttpServletRequest request, String message) {
         return StandardError.builder()
             .timeStamp(Instant.now())
             .status(status.value())
             .error(error)
-            .message("exception")
+            .message(message)
             .path(request.getRequestURI())
             .build();
     }
--- src/main/java/com/example/bootcamp/util/MessageUtils.java
+++ src/main/java/com/example/bootcamp/util/MessageUtils.java
@@ -5,4 +5,6 @@ public abstract class MessageUtils {
     public static final String ENTITY_NOT_FOUND = "Resource not found";
 
     public static final String STOCK_ALREADY_EXISTS = "Stock already exists in the database";
+
+    public static final String SERVER_ERROR = "Internal server error";
 }
